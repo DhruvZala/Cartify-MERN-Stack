242f2f8371bb71674aa28fa7b7af84a565af714f
Dhruv Zala <dhruvlaptop11@gmail.com>
2025-04-02 12:57:12 +0530

    UI Changes

--- src/components/product/Cart.tsx
+++ src/components/product/Cart.tsx
@@ -148,12 +148,12 @@ const CartPage: React.FC = () => {
                     Start adding some amazing products!
                   </p>
                   <div className="mt-6">
-                    <a
-                      href="/ProductPage"
+                    <Link
+                      to="/"
                       className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                       Continue Shopping
-                    </a>
+                    </Link>
                   </div>
                 </div>
               ) : (
